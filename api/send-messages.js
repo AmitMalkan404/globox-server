7f56c301562448ea93c2244aa86a41848db5fd98
Amitmalkan <amit.malkan@gmail.com>
2025-01-07 19:36:18 +0200

    fixed bug of multiple address checking of one package

--- api/send-messages.js
+++ api/send-messages.js
@@ -23,16 +23,30 @@ export default async function handler(req, res) {
         .json({ error: "Invalid input. Expected an array of messages." });
     }
 
-    let filteredPackages = packages.filter(pckg => {
-      return pckg.coordinates.length === 0
-    })
+    let filteredPackages = packages.filter((pckg) => {
+      return pckg.coordinates.length === 0;
+    });
 
-    const filteredMessages = mapMessagesWithFirebaseId(filteredPackages, messages);
+    const filteredMessages = mapMessagesWithFirebaseId(
+      filteredPackages,
+      messages
+    );
 
     var updatedPackages = {};
 
     try {
-      filteredMessages.forEach(async (pckg) => {
+      for (const pckg of filteredMessages) {
+        // if the package has an address then no need to check further
+        if (
+          updatedPackages[pckg.firebaseId] &&
+          updatedPackages[pckg.firebaseId].address &&
+          updatedPackages[pckg.firebaseId].address.length > 0
+        ) {
+          console.log(
+            `Skipping package ${pckg.firebaseId} as it already has an address.`
+          );
+          continue;
+        }
         let addressRes = extractAddressFromText(pckg.message);
 
         // if arrived
@@ -50,15 +64,15 @@ export default async function handler(req, res) {
             status: 1,
           };
         }
-      });
+      }
 
       for (const packageId in updatedPackages) {
-        let pckg = updatedPackages[packageId]
+        let pckg = updatedPackages[packageId];
 
         // if there is no address or coordinates already exist
         if (pckg.address === "" || pckg.coordinates.length === 2) continue;
-        let latLng = await getLatLngWithBing(pckg.address)
-        updatedPackages[packageId].coordinates = latLng
+        let latLng = await getLatLngWithBing(pckg.address);
+        updatedPackages[packageId].coordinates = latLng;
       }
     } catch (error) {
       console.error("Error Fetching Lat Lng of address: ", error);
@@ -66,12 +80,15 @@ export default async function handler(req, res) {
 
     await updateFirebaseData(updatedPackages);
 
-    res.status(200).json({ res: `packages updated successfully with ${Object.keys(updatedPackages).length}` });
+    res.status(200).json({
+      res: `packages updated successfully with ${
+        Object.keys(updatedPackages).length
+      }`,
+    });
   } else {
     res.status(405).json({ error: "Method not allowed" });
   }
 }
-
 
 export const mapMessagesWithFirebaseId = (packages, messages) => {
   // שליפת packageIdים מתוך החבילות יחד עם ה-Firebase ID
