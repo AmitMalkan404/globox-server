e6bf5d8caec5707ab7e6de5dd9db6512a56ce284
Amitmalkan <amit.malkan@gmail.com>
2025-01-10 21:25:04 +0200

    changed key from firebaseId to packageId + added air-conditioner preparation for deletion instead of archiving

--- api/archive-package.js
+++ api/archive-package.js
@@ -6,23 +6,47 @@ export default async function handler(req, res) {
       const packageToArchive = req.body;
 
       // בדיקת קלט
-      if (!packageToArchive.id) {
+      if (!packageToArchive.packageId) {
         return res.status(400).json({ error: "Invalid input." });
       }
 
-      const firebaseId = packageToArchive.id;
+      const packageId = packageToArchive.packageId;
 
-      const docRef = db.collection("packages").doc(firebaseId); // קישור למסמך לפי firebaseId
-      await docRef.update({ status: -1 });
+      // שליפת המסמך לפי packageId
+      const snapshot = await db
+        .collection("packages")
+        .where("packageId", "==", packageId)
+        .get();
 
-      res
-        .status(200)
-        .json({ message: `Document ${firebaseId} status updated successfully` });
+      // בדיקה אם המסמך קיים
+      if (snapshot.empty) {
+        console.log(`No document found with packageId: ${packageId}`);
+        res.status(500).json({ error: `Package ${packageId} not found` });
+        return;
+      }
+
+      // מחיקת כל המסמכים שמכילים את packageId (למקרה שיש יותר מאחד)
+      snapshot.forEach(async (doc) => {
+        
+        // await db.collection("packages").doc(doc.id).delete(); // זה בשביל למחוק
+        // res
+        //   .status(200)
+        //   .json({
+        //     message: `Document with ID ${doc.id} and packageId ${packageId} deleted successfully.`,
+        //   });
+
+        const docRef = db.collection("packages").doc(doc.id); // בשביל לעדכן ל1-
+        await docRef.update({ status: -1 }); // בשביל לעדכן ל1-
+        res
+          .status(200)
+          .json({
+            message: `Status for documents with packageId ${packageId} updated to -1.`,
+          });
+      });
     } catch (error) {
       console.error(`Error updating document ${firebaseId}:`, error);
-      res.status(500).json({ error: "Failed to update document" });
+      res.status(500).json({ error: "Failed to delete document" });
     }
-
   } else {
     res.status(405).json({ error: "Method not allowed" });
   }
